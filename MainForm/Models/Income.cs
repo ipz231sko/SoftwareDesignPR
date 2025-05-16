25799231a563cb7a2c9d57f5ea0fa6d4035f3c37
ipz231_sko <ipz231_sko@student.ztu.edu.ua>
2025-05-16 10:31:10 +0300

    feat: реалізовано модель транзакцій та інтерфейс репозиторію

---
+++ MainForm/Models/Income.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainForm.Models
+{
+    public class Income : Transaction
+    {
+        public override string Type => "Дохід";
+    }
+}
